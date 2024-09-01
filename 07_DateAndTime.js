69d14e0177c4826868854a3a161b24556c66a54c
subhakarr <subhankarr@outlook.com>
2024-09-01 16:23:55 -0400

    date time in js

---
+++ 07_DateAndTime.js
@@ -0,0 +1,12 @@
+// Dates in JS
+let date = new Date();
+// let dateToString=date.toString();
+// console.log(dateToString);
+// console.log(dateToString.split('G'))
+// let newdate=dateToString.split('G')
+// console.log(`the new converted date is ${newdate[0]}`)
+// // use of todatestring method
+console.log(` The new val for the method todatestring will ${date.toDateString()}`);
+console.log(` The new val for the method toJSON will ${date.toJSON()}`);
+console.log(` The new val for the method toLocaleTimeString will ${date.toLocaleTimeString()}`);
+console.log(` The new val for the method toLocaleString will ${date.toLocaleString()}`);
