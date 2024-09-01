cfca82c234b3a8f44fdfb4888eea43130ec4d3af
subhakarr <subhankarr@outlook.com>
2024-09-01 15:24:43 -0400

    num and math

---
+++ 06_num_math.js
@@ -0,0 +1,18 @@
+//numbers method 
+// tofixed()  to set delcimal places like 2 decimal places
+
+let num=100.45987
+console.log(`the length for the folowing ${num} is ${num.length}`)
+console.log(num.toFixed(2));
+
+// convert the num to string aten get the length
+let stringff= num.toString();
+console.log(stringff.length)
+//----------------------Math--------------------------------------------
+const hundreds=1000000
+console.log(Math)
+// Reversion of -ve value to +ve
+console.log(Math.abs(-9));
+// Round number 
+//console.log(Math.round(5.7))
+console.log(Math.round(num))
