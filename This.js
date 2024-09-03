e4387108705e12b7fdf5771987d688a69411e75a
subhakarr <subhankarr@outlook.com>
2024-09-03 14:42:41 -0400

    This and The Ternary Operator

---
+++ This.js
@@ -0,0 +1,12 @@
+function chain(){
+    console.log(this);
+}
+
+console.log(chain());
+//--------------------------------------
+const username="Subahnkar "
+//The Ternary Operator
+let x
+const answer=  x > 10 ? "Greater than 10 " : "less than 10 ";
+
+console.log(answer)
