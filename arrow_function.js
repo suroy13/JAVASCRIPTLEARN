8e857b360995d49fd0f4fd485bfabc8f98576fa2
subhakarr <subhankarr@outlook.com>
2024-09-03 15:39:55 -0400

    arrow function

---
+++ arrow_function.js
@@ -0,0 +1,7 @@
+
+const addnum =  (a,b) => (a+b) // the brackets here means it dont need return key word
+// the is called implicit return , the another where return keyword is used is called explicit return
+console.log(addnum(3,4));
+
+const Objreturn = () => ({user: "subhankar"})
+console.log(Objreturn());
