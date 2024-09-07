e15e286f0013c22fe554d74df3ab986774387471
subhakarr <subhankarr@outlook.com>
2024-09-07 02:39:24 -0400

    commit all changes

---
+++ LoopsInObject.js
@@ -0,0 +1,13 @@
+ const myObject ={
+    frontend: "html",
+    backend: "Sql",
+    Scripting: "javascript"
+ }
+
+ for (const key in myObject) {
+    
+    console.log(`The values in the objects are ${myObject[key]}`);
+ }
+ // to print only the keys
+
+ 
---
+++ forEachLoop.js
@@ -0,0 +1,12 @@
+const country=["india","canada","USA","China","Japan"]
+
+// country.forEach(function (countryValues){  // Callback function means its dont have any name
+
+//     console.log(countryValues);
+// })
+// Using the arrw function
+
+country.forEach( (countryValues )=> {  // Callback function means its dont have any name
+
+    console.log(countryValues);
+})
