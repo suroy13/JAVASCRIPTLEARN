6341b19bff0e2a22e55dbf0561431d3d002079fd
subhakarr <subhankarr@outlook.com>
2024-09-08 04:02:38 -0400

    Other files commit

--- 04_Basic_function_Scopes.js
+++ 04_Basic_function_Scopes.js
@@ -1,1 +1,7 @@
-file name changed 
+//OTP generator
+let otp;
+let otpRandomGenerator=Math.random();
+otp = Math.floor(otpRandomGenerator * 10000).toString().padStart(4, '0');
+console.log(`The generated OTP is ${otp}`);
+
+
--- forEachLoop.js
+++ forEachLoop.js
@@ -6,7 +6,26 @@ const country=["india","canada","USA","China","Japan"]
 // })
 // Using the arrw function
 
-country.forEach( (countryValues )=> {  // Callback function means its dont have any name
+// country.forEach( (countryValues )=> {  // Callback function means its dont have any name
 
-    console.log(countryValues);
+//     console.log(countryValues);
+// })
+
+//loop through arry of objects for specific value
+
+const car=[
+    { nameofCar: "BMW",
+    countryOfManufacture: "Germany"},
+
+    { nameofCar: "SUZUKI",
+        countryOfManufacture: "Japan"},
+
+        { nameofCar: "TATA",
+            countryOfManufacture: "India"}
+]
+
+car.forEach((itemsOfCar) => {
+console.log(itemsOfCar.countryOfManufacture);
 })
+
+
